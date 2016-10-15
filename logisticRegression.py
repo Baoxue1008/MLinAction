2cbbcc05d006433c0f0d6cb0566fd723b9fbc93e
Baoxue1008 <442661063@qq.com>
2016-10-15 17:31:11 +0800

    20161015

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,5 +1,5 @@
 from numpy import *
-
+import random
 
 def loadDataSet():
     dataMat = []; labelMat = []
@@ -25,6 +25,66 @@ def gradAscent(dataMat,dataLabel):
     for i in range(maxCycles):
         h = sigmoid(dataMat * weight)
         error = dataLabel - h
-        weight = weight + alpha *
+        weight = weight + alpha * dataMat.transpose() * error
+
+    return weight
+
+def stocGradAscent0(dataMat,dataLabel):
+    dataMat = array(dataMat)
+    m,n = shape(dataMat)
+    weight = ones(n)
+    alpha = 0.01
+    for i in range(m):
+        h = sigmoid(sum(dataMat[i] * weight))
+        error = dataLabel[i] - h
+        weight = weight + alpha *  dataMat[i]
+    return weight
+
+def stocGradAscent1(dataMat,dataLabel,numIter=150):
+    dataMat = array(dataMat)
+    m,n = shape(dataMat)
+    weights = ones(n)
+
+    for i in range(numIter):
+        indexs = range(m)
+        for j in range(m):
+            alpha = 0.01 + 4.0/(i + j + 1)
+            cur = int(random.uniform(0,len(indexs)))
+            h = sigmoid(sum(dataMat[cur]*weights))
+            error = dataLabel[cur] - h
+            weights = weights + alpha * error * dataMat[cur]
+            del(indexs[cur])
+
+    return weights
+
+
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat,labelMat=loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i])== 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1'); plt.ylabel('X2');
+    plt.show()
+
+if __name__ == '__main__':
+    a,b = loadDataSet()
+    x,y = shape(a)
+    weights = stocGradAscent1(a,b)
+
+    plotBestFit(weights)
 
 
